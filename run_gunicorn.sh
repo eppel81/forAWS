0036dda6682337b6aea5dae0e6db47d81b6ecf80
dmitriy <dege81@gmail.com>
2017-02-27 12:17:24 +0200

    change manage.py to prod

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
-PROJECT_BASE=/home/awsuser
+PROJECT_BASE=/home/ubuntu/web
 VENV=$PROJECT_BASE/virtualenv
 LOGFILE=$PROJECT_BASE/shared/logs/victoria-gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=2
 
 HOST=127.0.0.1
-PORT=8000
+PORT=8002
 
 HOSTNAME=$(hostname -f)
 
@@ -19,5 +19,5 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 export DJANGO_SETTINGS_MODULE=forAWS.settings.prod
 
 cd $PROJECT_BASE/current
-exec gunicorn -w $NUM_WORKERS -b $HOST:$PORT victoria.wsgi:application \
+exec gunicorn -w $NUM_WORKERS -b $HOST:$PORT settings.wsgi:application \
         --timeout=600 --log-level=error
