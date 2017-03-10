966ff2c2d2dd9a869361fbe65192a9484f6ab5b9
dmitriy <dege81@gmail.com>
2017-03-10 16:01:32 +0200

    fix run_gunicorn.sh

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -18,6 +18,6 @@ test -d $LOGDIR || mkdir -p $LOGDIR
 
 export DJANGO_SETTINGS_MODULE=settings.prod
 
-cd $PROJECT_BASE/current
+cd $PROJECT_BASE/forAWS
 exec gunicorn -w $NUM_WORKERS -b $HOST:$PORT settings.wsgi:application \
         --timeout=600 --log-level=error
