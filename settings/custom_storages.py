c1cd3e3b4650c1312f49b081b0b711a3b49c45d9
dmitriy <dege81@gmail.com>
2017-03-10 15:06:24 +0200

    add region to custom storage

--- settings/custom_storages.py
+++ settings/custom_storages.py
@@ -1,10 +1,12 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 
-class StaticFilesStorage(S3BotoStorage):
+class StaticFilesStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    region_name = 'eu-west-2'
 
 
-class MediaFilesStorage(S3BotoStorage):
+class MediaFilesStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    region_name = 'eu-west-2'
